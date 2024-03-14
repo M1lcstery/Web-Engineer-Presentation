512eb1be15aa67635c6f49129489258f4a675602
M1stery <lautarocepero3@gmail.com>
2024-03-14 10:03:44 -0300

    added main index.html and css

--- we_app/mix.exs
+++ we_app/mix.exs
@@ -57,6 +57,7 @@ defmodule WeApp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
+      {:kex, "~> 0.16.0"},
       {:bandit, "~> 1.2"}
     ]
   end
